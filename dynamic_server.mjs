3011b39a2a142d3003b16320a686daad5f889863
ethanjohnson <john64862@stthomas.edu>
2023-11-01 12:59:45 -0500

    Installed npm chart.js

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -129,13 +129,13 @@ app.get("/year/:release_year", (req, res) => {
             let next_year = input_year + 1;
             if (next_year === 2011) {next_year = 2001;}
             let next_text = "Go to songs from " + next_year;
-            let next_address = "http://localhost:9000/year/" + next_year;
+            let next_address = "https://cwcarlin-webdev-dynamic.onrender.com/year/" + next_year;
             response = response.replace('$$NEXT_TEXT$$', next_text).replace('$$NEXT_ADDRESS$$', next_address);
             //Create previous link
             let prev_year = input_year - 1;
             if (prev_year === 2000) {prev_year = 2010;}
             let prev_text = "Go to songs from " + prev_year;
-            let prev_address = "http://localhost:9000/year/" + prev_year;
+            let prev_address = "https://cwcarlin-webdev-dynamic.onrender.com/year/" + prev_year;
             response = response.replace('$$PREV_TEXT$$', prev_text).replace('$$PREV_ADDRESS$$', prev_address);
             //Send Response
             res.status(200).type('html').send(response);
